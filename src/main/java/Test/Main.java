dc3ef7d82cd629052567f14db22972c1b0164f51
Amer4672 <amer.haqim05@gmail.com>
2025-08-21 22:09:53 +0800

    -new method that grabs chapter links from the main page into txt file -new method that grabs chapter titles from main page into txt file -new method that uses links from the 2 files mentioned above to download chapter -the og method dont work for some reason idk -main args method asks for 2 options for the 3 new methods mentioned

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -7,28 +7,67 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-       Scanner in = new Scanner(System.in);
         String userHome = System.getProperty("user.home");
 
         NovelBinDowloader nbd = new NovelBinDowloader();
         WebDriver driver = nbd.chromeDriverSetup();
 
-        System.out.print("Enter novel title: ");
-        String saveDir = userHome + "\\Desktop\\downloaded_novels\\" + in.nextLine().trim() + "\\";
-        new File(saveDir).mkdirs();
+        System.out.println("1. Download all links\n2. Download all titles\n3. Download chapters");
+        int options = in.nextInt();
+        in.nextLine();
 
-        System.out.print("Enter link: ");
-        String link = in.nextLine();
+        if (options == 1) {
+            System.out.print("Enter file name: ");
+            String fileName = in.nextLine();
+            System.out.print("Enter link: ");
+            String link = in.nextLine();
 
-        nbd.downloadChapter(driver, link);
-/*
-        nbd.webAccessor(link, driver);
-        try {
-            nbd.downloadAllChapters(driver);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            nbd.webAccessor(link, driver);
+
+            try {
+                nbd.allLinksToTxt(driver, fileName);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            nbd.webCloser(driver);
         }
-        nbd.webCloser(driver);*/
+
+        else if (options == 2) {
+            System.out.print("Enter file name: ");
+            String fileName = in.nextLine();
+            System.out.print("Enter link: ");
+            String link = in.nextLine();
+
+            nbd.webAccessor(link, driver);
+
+            try {
+                nbd.titleDownloader(driver, fileName);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            nbd.webCloser(driver);
+
+        }
+
+        else if (options == 3) {
+            System.out.print("Enter novel title: ");
+            String novelTitle = in.nextLine();
+            String saveDir = userHome + "\\Desktop\\downloaded novels\\" + novelTitle + "\\";
+            new File(saveDir).mkdirs();
+            System.out.print("Enter file name for chapter content link: ");
+            String fileNameLink = in.nextLine();
+            System.out.print("Enter file name for chapter title: ");
+            String fileNameTitle = in.nextLine();
+
+            try {
+                nbd.downloadAllChaptersHTMLver2(driver, saveDir, fileNameLink, fileNameTitle);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        nbd.webCloser(driver);
     }
 }
--- src/main/java/Test/NovelBinDowloader.java
+++ src/main/java/Test/NovelBinDowloader.java
@@ -5,18 +5,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class NovelBinDowloader {
+
+    //webdriver stuff///////////////////////////////////////////////////////////////////////////////////////
 
     public WebDriver chromeDriverSetup() {
         System.setProperty("webdriver.chrome.driver", "C:\\Browser driver\\chromedriver.exe");
@@ -28,48 +29,37 @@ public class NovelBinDowloader {
         return driver;
     }
 
-    public void waitDriver(WebDriver driver) {
+    public WebDriverWait waitDriver(WebDriver driver) { /*WebDriverWait*/
         Random rand = new Random();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(rand.nextInt(10) + 10));
+        return wait;//return wait so that i can do expected conditions
     }
 
     public void webAccessor(String url, WebDriver driver) {
         driver.get(url);
         driver.manage().window().maximize();
-        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         waitDriver(driver);
-
     }
 
     public void webCloser(WebDriver driver) {
         driver.quit();
     }
 
-    /*/outdated method
-    public void chapterDownloader(WebDriver driver, int chapNum, String title) {
-        driver.findElement(By.cssSelector("a[href*='/chapter-" + chapNum + "']")).click();
-        //String text = driver.findElement(By.tagName("body")).getText();
 
-        List<WebElement> paragraphs = driver.findElements(By.tagName("p"));
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Chapter-" + chapNum + ".txt"))) {
-            for (WebElement p : paragraphs) {
-                writer.write(p.getText().trim());
-                writer.newLine(); // adds line break between paragraphs
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        driver.navigate().back();
-    }*/
 
-    public void downloadAllChapters(WebDriver driver) throws InterruptedException {
+    //download chapter methods///////////////////////////////////////////////////////////////////////////////////////////
+
+  /*  public void downloadAllChaptersHTML(WebDriver driver, String path) throws InterruptedException {
+
         //Find all chapter links
+        waitDriver(driver);
+        //waitDriver(driver).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("a[href*='/chapter-']")));
         List<WebElement> chapterElements = driver.findElements(By.cssSelector("a[href*='/chapter-']"));
         waitDriver(driver);
 
         List<String> chapterLinks = new ArrayList<>();
         List<String> chapterTitle = new ArrayList<>();
+
         for (WebElement el : chapterElements) {
             String href = el.getAttribute("href");
             if (href != null && !href.isEmpty()) {
@@ -81,11 +71,13 @@ public class NovelBinDowloader {
             }
         }
 
-        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         waitDriver(driver);
 
+        for (int i = 2; i < 4; i++) { //int i = 2 to skip the read now and latest chapter //chapterLinks.size()
 
-        for (int i = 0; i < 3/*chapterLinks.size()*/; i++) {
+            System.out.println(chapterTitle.get(i));
+            System.out.println(chapterLinks.get(i));
+
             driver.get(chapterLinks.get(i));
             waitDriver(driver);
 
@@ -93,12 +85,13 @@ public class NovelBinDowloader {
             List<WebElement> paragraphs = driver.findElements(By.tagName("p"));
 
             // Save to file
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(chapterTitle.get(i) + ".txt"))) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path + chapterTitle.get(i) + " (HTML).txt"))) {
                 for (WebElement p : paragraphs) {
-                    String text = p.getText().trim();
+                    String text = p.getAttribute("outerHTML");
                     if (!text.isEmpty()) {
                         writer.write(text);
                         writer.newLine();
+                        writer.write("<br>");
                         writer.newLine();
                     }
                 }
@@ -107,9 +100,57 @@ public class NovelBinDowloader {
                 e.printStackTrace();
             }
         }
+    }*/
+
+    public void downloadAllChaptersHTMLver2(WebDriver driver, String path, String fileNameLink, String fileNameTitle) throws InterruptedException {
+        List<String> chapLinks = new ArrayList<>();
+        List<String> chapTitle = new ArrayList<>();
+
+        //chap link into list
+        try (BufferedReader reader = new BufferedReader(new FileReader( fileNameLink + ".txt"))) {
+            String link;
+            while ((link = reader.readLine()) != null) {
+                chapLinks.add(link);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //chap title into list
+        try (BufferedReader reader = new BufferedReader(new FileReader( fileNameTitle + ".txt"))) {
+            String title;
+            while ((title = reader.readLine()) != null) {
+                chapTitle.add(title);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 2; i < 4; i++) { //chapLinks.size()
+            Thread.sleep(10000);
+            driver.get(chapLinks.get(i));
+            // Collect all <p> text
+            List<WebElement> paragraphs = driver.findElements(By.tagName("p"));
+
+            // Save to file
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(path + chapTitle.get(i) + " (HTML).txt"))) {
+                for (WebElement p : paragraphs) {
+                    String text = p.getAttribute("outerHTML");
+                    if (!text.isEmpty()) {
+                        writer.write(text);
+                        writer.newLine();
+                        writer.write("<br>");
+                        writer.newLine();
+                    }
+                }
+                System.out.println("✅ " + chapTitle.get(i) + " saved.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void downloadChapter(WebDriver driver, String url) {
+    public void downloadIndivChapterHTML(WebDriver driver, String url) {
         driver.get(url);
         driver.manage().window().maximize();
         waitDriver(driver);
@@ -120,10 +161,11 @@ public class NovelBinDowloader {
         // Save to file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter( "Chapter" + chapNum + ".txt"))) {
             for (WebElement p : paragraphs) {
-                String text = p.getText().trim();
+                String text = p.getAttribute("outerHTML");
                 if (!text.isEmpty()) {
                     writer.write(text);
                     writer.newLine();
+                    writer.write("<br>");
                     writer.newLine();
                 }
             }
@@ -134,4 +176,61 @@ public class NovelBinDowloader {
 
     }
 
+    public void allLinksToTxt(WebDriver driver, String fileName) throws InterruptedException {
+        //Find all chapter links
+        Thread.sleep(10000);
+        //waitDriver(driver).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("a[href*='/chapter-']")));
+        List<WebElement> chapterElements = driver.findElements(By.cssSelector("a[href*='/chapter-']"));
+        waitDriver(driver);
+
+        List<String> chapterLinks = new ArrayList<>();
+
+        for (WebElement el : chapterElements) {
+            String href = el.getAttribute("href");
+            if (href != null && !href.isEmpty()) {
+                chapterLinks.add(href);
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + ".txt"))) {
+            for (String link : chapterLinks) {
+                if (!link.isEmpty()) {
+                    writer.write(link);
+                    writer.newLine();
+                }
+            }
+           System.out.println("✅ " + fileName + " saved.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void titleDownloader(WebDriver driver, String fileName) throws InterruptedException {
+        //Find all chapter links
+        Thread.sleep(10000);
+        //waitDriver(driver).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("a[href*='/chapter-']")));
+        List<WebElement> chapterElements = driver.findElements(By.cssSelector("a[href*='/chapter-']"));
+        waitDriver(driver);
+
+        List<String> chapterTitle = new ArrayList<>();
+
+        for (WebElement el : chapterElements) {
+            String title = el.getText().replaceAll("[\\\\/:*?\"<>|]", "");
+            if (title != null && !title.isEmpty()) {
+                chapterTitle.add(title);
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + ".txt"))) {
+            for (String title : chapterTitle) {
+                if (!title.isEmpty()) {
+                    writer.write(title);
+                    writer.newLine();
+                }
+            }
+            System.out.println("✅ " + fileName + " saved.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
